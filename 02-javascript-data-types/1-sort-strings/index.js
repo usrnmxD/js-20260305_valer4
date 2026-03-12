4843a2f2669733107fda18a7e364536d67fd95b0
val4git <val4git@gmail.com>
2026-03-12 10:57:34 +0300

    add 2nd task

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,14 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  const result = [...arr];
 
+  switch (param) {
+    case 'asc':
+      return result.sort((a, b) => a.localeCompare(b, 'ru', { caseFirst: 'upper' }));
+    case 'desc':
+      return result.sort((a, b) => b.localeCompare(a, 'ru', { caseFirst: 'upper' }));
+    default:
+      return result;
+  }
 }
